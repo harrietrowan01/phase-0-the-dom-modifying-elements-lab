36fe7313e122a309ad4893b7e3007eefcb3d1ac8
harrietrowan01 <harrietrowan01@gmail.com>
2021-06-11 02:33:11 -0400

    commit tree 783d7e65a35d3172f097a495924abb6a84c8bd90

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+//const ul = document.getElementById("main");
+//ul.remove("main");
+//const newHeader = document.createElement("H1");
+//document.body.appendChild("H1");
+
+document.querySelector("main").remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Harriet is the champion";
+
+
